202b188047cce26393ee8eebf9fa7eff4b64ed24
ESSch <ESSchtolts@yandex.ru>
2018-12-18 10:59:53 +0300

    Easy cluster creation

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 NAME_PROJECT=bitrix-123458;
 NAME_CLUSTER=bitrix;
 
-gcloud projects create $NAME_PROJECT --name $NAME_CLUSTER --enable-cloud-apis \
-&& gcloud config set project $NAME_PROJECT \
-&& gcloud projects list \
-&& gcloud container clusters create $NAME_CLUSTER --zone europe-north1-a
+echo "\nCreate a new project: enter 'Y'?";
+read a;
+if [ "$a" = "Y" ]
+then
+  gcloud projects create $NAME_PROJECT --name $NAME_CLUSTER --enable-cloud-apis \
+  && gcloud config set project $NAME_PROJECT \
+  && gcloud projects list;
+  gcloud container clusters get-credentials $NAME_CLUSTER --zone europe-north1-a --project $NAME_PROJECT
+fi;
+
+gcloud container clusters create $NAME_CLUSTER --zone europe-north1-a
