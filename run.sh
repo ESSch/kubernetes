a471a3cdfb289dba0b9dc2ea9ed601b9ac33c1ee
ESSch <ESSchtolts@yandex.ru>
2018-12-18 10:30:52 +0300

    A project create

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+NAME_PROJECT=bitrix-123458;
 NAME_CLUSTER=bitrix;
 
-gcloud container clusters create $NAME_CLUSTER --zone europe-north1-a
+gcloud projects create $NAME_PROJECT --name $NAME_CLUSTER --enable-cloud-apis \
+&& gcloud config set project $NAME_PROJECT \
+&& gcloud projects list \
+&& gcloud container clusters create $NAME_CLUSTER --zone europe-north1-a
