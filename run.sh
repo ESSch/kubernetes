820090e3c5d73ddf7bff1a796d7c6982180a1007
ESSch <ESSchtolts@yandex.ru>
2018-12-18 11:57:17 +0300

    A cluster reduce

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 NAME_PROJECT=bitrix-123458;
 NAME_CLUSTER=bitrix;
 
-echo "\nCreate a new project: enter 'Y'?";
+echo "Create a new project: enter 'Y'?";
 read a;
 if [ "$a" = "Y" ]
 then
@@ -11,4 +11,10 @@ then
   gcloud container clusters get-credentials $NAME_CLUSTER --zone europe-north1-a --project $NAME_PROJECT
 fi;
 
-gcloud container clusters create $NAME_CLUSTER --zone europe-north1-a
+gcloud container clusters create $NAME_CLUSTER \
+  --machine-type=n1-standard-1 --disk-size=10GB --image-type ubuntu \
+  --scopes compute-rw,gke-default \
+  --machine-type=custom-1-1024 \
+  --cluster-version=1.11 --enable-autoupgrade \
+  --num-nodes=2 --enable-autoscaling --min-nodes=1 --max-nodes=2 \
+  --zone europe-north1-a
